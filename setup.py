50a3331f7b6f8dd6fef931a74e1177788f0e60ac
glyph <>
2005-10-10 22:56:52 +0000

    distutils magic and package version information

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from distutils.core import setup
+
+setup(
+    name="Epsilon",
+    version="0.1",
+    maintainer="Divmod, Inc.",
+    maintainer_email="support@divmod.org",
+    url="http://divmod.org/trac/wiki/EpsilonProject",
+    license="MIT",
+    platforms=["any"],
+    description="A set of utility modules used by Divmod projects",
+    classifiers=(
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Utilities"),
+
+    packages=['epsilon',
+              'epsilon.test'])
