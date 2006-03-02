7382ca68147f8108db9f359d7e9c3d6c17c5fdb9
exarkun <>
2006-03-02 16:54:46 +0000

    revert indexer-subprocess-655-3

--- epsilon/hotfix.py
+++ epsilon/hotfix.py
@@ -14,6 +14,12 @@ def require(packageName, fixName):
         if filepath.FilePath('a') != filepath.FilePath('a'):
             from epsilon.hotfixes import filepath_copyTo
             filepath_copyTo.install()
+    elif (packageName, fixName) == ('twisted', 'stdio_getPeer'):
+        from twisted.internet import stdio
+        if 'getPeer' not in vars(stdio.StandardIO):
+            from epsilon.hotfixes import stdio_getPeer
+            print 'Installing getpeer fix'
+            stdio_getPeer.install()
     else:
         raise NoSuchHotfix(packageName, fixName)
 
