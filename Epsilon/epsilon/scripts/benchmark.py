44416f0e7872d7dbbca258ba82063e634277a038
exarkun <>
2006-05-28 16:37:32 +0000

    Revert r6825 - causes regression in an axiomatic plugin from sine
    Refs #1056
    
    sine.axiom.plugins.confessionapp still imports Juice-related modules from Vertex.

--- Epsilon/epsilon/scripts/benchmark.py
+++ Epsilon/epsilon/scripts/benchmark.py
@@ -15,8 +15,8 @@ from twisted.protocols import policies
 import epsilon
 from epsilon import structlike
 
-from epsilon import juice
-from epsilon.test import utils
+from vertex import juice
+from vertex.test import utils
 
 
 class diskstat(structlike.record(
