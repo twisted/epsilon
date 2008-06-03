6197fd5f302f943188d36461c8bf8bde6099505e
glyph <>
2008-06-03 02:05:16 +0000

    give epsilon.structlike.record a pretty string representation
    Author: glyph
    
    Reviewer: pjd
    
    Fixes #2591
    
    This `__repr__` will render all the known attributes and the class name of the given `structlike.record` subclass.

--- Epsilon/epsilon/structlike.py
+++ Epsilon/epsilon/structlike.py
@@ -1,5 +1,68 @@
 # -*- test-case-name: epsilon.test.test_structlike -*-
 
+"""
+This module implements convenience objects for classes which have initializers
+and repr()s that describe a fixed set of attributes.
+"""
+
+from twisted.python import context
+
+_NOT_SPECIFIED = object()
+
+
+class _RecursiveReprer(object):
+    """
+    This object maintains state so that repr()s can tell when they are
+    recursing and not do so.
+    """
+    def __init__(self):
+        self.active = {}
+
+    def recursiveRepr(self, stuff, thunk=repr):
+        """
+        Recursive repr().
+        """
+        ID = id(stuff)
+        if ID in self.active:
+            return '%s(...)' % (stuff.__class__.__name__,)
+        else:
+            try:
+                self.active[ID] = stuff
+                return thunk(stuff)
+            finally:
+                del self.active[ID]
+
+
+def _contextualize(contextFactory, contextReceiver):
+    """
+    Invoke a callable with an argument derived from the current execution
+    context (L{twisted.python.context}), or automatically created if none is
+    yet present in the current context.
+
+    This function, with a better name and documentation, should probably be
+    somewhere in L{twisted.python.context}.  Calling context.get() and
+    context.call() individually is perilous because you always have to handle
+    the case where the value you're looking for isn't present; this idiom
+    forces you to supply some behavior for that case.
+
+    @param contextFactory: An object which is both a 0-arg callable and
+    hashable; used to look up the value in the context, set the value in the
+    context, and create the value (by being called).
+
+    @param contextReceiver: A function that receives the value created or
+    identified by contextFactory.  It is a 1-arg callable object, called with
+    the result of calling the contextFactory, or retrieving the contextFactory
+    from the context.
+    """
+    value = context.get(contextFactory, _NOT_SPECIFIED)
+    if value is not _NOT_SPECIFIED:
+        return contextReceiver(value)
+    else:
+        return context.call({contextFactory: contextFactory()},
+                            _contextualize, contextFactory, contextReceiver)
+
+
+
 class StructBehavior(object):
     __names__ = []
     __defaults__ = []
@@ -9,7 +72,9 @@ class StructBehavior(object):
 
         # Turn all the args into kwargs
         if len(args) > len(self.__names__):
-            raise TypeError("Got %d positional arguments but expected no more than %d" % (len(args), len(self.__names__)))
+            raise TypeError(
+                "Got %d positional arguments but expected no more than %d" %
+                (len(args), len(self.__names__)))
 
         for n, v in zip(self.__names__, args):
             if n in kw:
@@ -30,7 +95,24 @@ class StructBehavior(object):
             raise TypeError('Got unexpected arguments: ' + ', '.join(kw))
 
 
-_NOT_SPECIFIED = object()
+    def __repr__(self):
+        """
+        Generate a string representation.
+        """
+
+        def doit(rr):
+            def _recordrepr(self2):
+                """
+                Internal implementation of repr() for this record.
+                """
+                return '%s(%s)' % (
+                    self.__class__.__name__,
+                    ', '.join(["%s=%s" %
+                               (n, repr(getattr(self, n, None)))
+                               for n in self.__names__]))
+            return rr.recursiveRepr(self, _recordrepr)
+        return _contextualize(_RecursiveReprer, doit)
+
 
 def record(*a, **kw):
     """
@@ -72,7 +154,8 @@ def record(*a, **kw):
         attributeNames = kw.keys()
         attributeNames.sort()
     else:
-        raise TypeError("record must be called with zero or one positional arguments")
+        raise TypeError(
+            "record must be called with zero or one positional arguments")
 
     # Work like Python: allow defaults specified backwards from the end
     defaults = []
@@ -80,7 +163,10 @@ def record(*a, **kw):
         default = kw.pop(attributeName, _NOT_SPECIFIED)
         if defaults:
             if default is _NOT_SPECIFIED:
-                raise TypeError("You must specify default values like in Python; backwards from the end of the argument list, with no gaps")
+                raise TypeError(
+                    "You must specify default values like in Python; "
+                    "backwards from the end of the argument list, "
+                    "with no gaps")
             else:
                 defaults.append(default)
         elif default is not _NOT_SPECIFIED:
--- Epsilon/epsilon/test/test_structlike.py
+++ Epsilon/epsilon/test/test_structlike.py
@@ -1,6 +1,21 @@
 
+"""
+Tests for L{epsilon.structlike}.
+"""
+
+import threading
+
 from twisted.trial import unittest
+from twisted.internet.threads import deferToThread
+from twisted.internet.defer import gatherResults
 from epsilon.structlike import record
+
+
+class MyRecord(record('something somethingElse')):
+    """
+    A sample record subclass.
+    """
+
 
 
 class StructLike(unittest.TestCase):
@@ -70,3 +85,102 @@ class StructLike(unittest.TestCase):
         R = record('x')
         self.assertRaises(TypeError, R, x=5, y=6)
         self.assertRaises(TypeError, R, 5, 6)
+
+
+    def test_typeStringRepresentation(self):
+        """
+        'Record' types should have a name which provides information about the
+        slots they contain.
+        """
+        R = record('xyz abc def')
+        self.assertEquals(R.__name__, "Record<xyz abc def>")
+
+
+    def test_instanceStringRepresentation(self):
+        """
+        'Record' instances should provide a string representation which
+        provides information about the values contained in their slots.
+        """
+        obj = MyRecord(something=1, somethingElse=2)
+        self.assertEquals(repr(obj), 'MyRecord(something=1, somethingElse=2)')
+
+
+    def test_instanceStringRepresentationNesting(self):
+        """
+        Nested L{Record} instances should have nested string representations.
+        """
+        obj = MyRecord(something=1, somethingElse=2)
+        objRepr = 'MyRecord(something=1, somethingElse=2)'
+        self.assertEquals(
+            repr(MyRecord(obj, obj)),
+            'MyRecord(something=%s, somethingElse=%s)' % (objRepr, objRepr))
+
+
+    def test_instanceStringRepresentationRecursion(self):
+        """
+        'Record' instances should provide a repr that displays 'ClassName(...)'
+        when it would otherwise infinitely recurse.
+        """
+        obj = MyRecord(something=1, somethingElse=2)
+        obj.somethingElse = obj
+        self.assertEquals(
+            repr(obj), 'MyRecord(something=1, somethingElse=MyRecord(...))')
+
+
+    def test_instanceStringRepresentationUnhashableRecursion(self):
+        """
+        'Record' instances should display 'ClassName(...)' even for unhashable
+        objects.
+        """
+        obj = MyRecord(something=1, somethingElse=[])
+        obj.somethingElse.append(obj)
+        self.assertEquals(
+            repr(obj), 'MyRecord(something=1, somethingElse=[MyRecord(...)])')
+
+
+    def test_threadLocality(self):
+        """
+        An 'Record' repr()'d in two separate threads at the same time should
+        look the same (i.e. the repr state tracking for '...' should be
+        thread-local).
+        """
+        class StickyRepr(object):
+            """
+            This has a __repr__ which will block until a separate thread
+            notifies it that it should return.  We use this to create a race
+            condition.
+            """
+            waited = False
+            def __init__(self):
+                self.set = threading.Event()
+                self.wait = threading.Event()
+            def __repr__(self):
+                if not self.waited:
+                    self.set.set()
+                    self.wait.wait()
+                return 'sticky'
+        r = StickyRepr()
+        mr = MyRecord(something=1, somethingElse=r)
+        d = deferToThread(repr, mr)
+        def otherRepr():
+            # First we wait for the first thread doing a repr() to enter its
+            # __repr__()...
+            r.set.wait()
+            # OK, now it's blocked.  Let's make sure that subsequent calls to
+            # this repr() won't block.
+            r.waited = True
+            # Do it!  This is a concurrent repr().
+            result = repr(mr)
+            # Now we're done, wake up the other repr and let it complete.
+            r.wait.set()
+            return result
+        d2 = deferToThread(otherRepr)
+
+        def done((thread1repr, thread2repr)):
+            knownGood = 'MyRecord(something=1, somethingElse=sticky)'
+            # self.assertEquals(thread1repr, thread2repr)
+            self.assertEquals(thread1repr, knownGood)
+            self.assertEquals(thread2repr, knownGood)
+        return gatherResults([d, d2]).addCallback(done)
+
+
