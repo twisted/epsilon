7084db1eeecbea821e24b854a7b2cc1ed050135c
Laurens Van Houtven <_@lvh.io>
2014-01-12 11:53:44 +0100

    Use setuptools for setup.py, specify dependencies, don't rely on Twisted for egg_info

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
+from setuptools import setup, find_packages
+import re
 
-from epsilon import version, setuphelper
+versionPattern = re.compile(r"""^__version__ = ['"](.*?)['"]$""", re.M)
+with open("epsilon/_version.py", "rt") as f:
+    version = versionPattern.search(f.read()).group(1)
 
-setuphelper.autosetup(
+setup(
     name="Epsilon",
-    version=version.short(),
+    version=version,
+    description="A set of utility modules used by Divmod projects",
+
     maintainer="divmod-dev",
     maintainer_email="divmod-dev@lists.launchpad.net",
     url="https://launchpad.net/divmod.org",
+
+    install_requires=[
+        "Twisted>=13.2.0",
+        "PyOpenSSL>=0.13"
+    ],
+    packages=find_packages(),
+    scripts=['bin/benchmark'],
+
     license="MIT",
     platforms=["any"],
-    description="A set of utility modules used by Divmod projects",
+
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Twisted",
@@ -18,5 +32,4 @@ setuphelper.autosetup(
         "Programming Language :: Python",
         "Topic :: Internet",
         "Topic :: Security",
-        "Topic :: Utilities"],
-    scripts=['bin/benchmark'])
+        "Topic :: Utilities"])
