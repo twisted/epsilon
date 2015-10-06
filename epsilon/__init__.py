c824e034896516bb76f092472e8d926efe5f40ab
Tristan Seligmann <mithrandi@mithrandi.net>
2015-10-06 21:19:01 +0200

    Handle non-release versions properly.

--- epsilon/__init__.py
+++ epsilon/__init__.py
@@ -6,7 +6,8 @@ del get_versions
 from twisted.python import versions
 
 def asTwistedVersion(packageName, versionString):
-    return versions.Version(packageName, *map(int, versionString.split(".")))
+    return versions.Version(
+        packageName, *map(int, versionString.split('+', 1)[0].split(".")))
 
 version = asTwistedVersion("epsilon", __version__)
 del versions
