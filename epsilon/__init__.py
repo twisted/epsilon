d4500d551e9afcea3ffd5100bef0b0c747e38047
Tristan Seligmann <mithrandi@mithrandi.net>
2020-08-05 11:06:14 +0200

    Switch to setuptools_scm.

--- epsilon/__init__.py
+++ epsilon/__init__.py
@@ -1,14 +1,12 @@
 # -*- test-case-name: epsilon.test -*-
-from epsilon._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
-
-from twisted.python import versions
+from epsilon._version import __version__
 
 def asTwistedVersion(packageName, versionString):
+    from twisted.python import versions
+    import re
     return versions.Version(
-        packageName, *map(int, versionString.split('+', 1)[0].split(".")))
+        packageName,
+        *map(int, re.match(r"[0-9.]*", versionString).group().split(".")[:3]))
 
 version = asTwistedVersion("epsilon", __version__)
-
 __all__ = ['__version__', 'version']
