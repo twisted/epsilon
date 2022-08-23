56b678fa53c5bb872ae40d07f67a57f8158ec305
Glyph <glyph@twistedmatrix.com>
2022-08-22 23:03:50 -0700

    fix certcreate to work on python3
    use 'open' since 'file' hasn't been around for a decade now
    
    fixes #39

--- epsilon/scripts/certcreate.py
+++ epsilon/scripts/certcreate.py
@@ -38,7 +38,7 @@ def createSSLCertificate(opts):
         sslopt[y] = opts[x]
     serialNumber = int(opts['serial-number'])
     ssc = KeyPair.generate().selfSignedCert(serialNumber, **sslopt)
-    file(opts['filename'], 'w').write(ssc.dumpPEM())
+    open(opts['filename'], 'wb').write(ssc.dumpPEM())
     if not opts['quiet']:
         print('Wrote SSL certificate:')
         print(ssc.inspect())
